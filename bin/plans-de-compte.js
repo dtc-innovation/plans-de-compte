43d11e3ff817577031a1c651d29a7ee221b0fdb4
David Bruant <bruant.d@gmail.com>
2019-02-21 23:10:44 +0100

    First working version

--- bin/plans-de-compte.js
+++ bin/plans-de-compte.js
@@ -1,6 +1,7 @@
 import {join, isAbsolute, relative} from 'path';
 
-import { readdir, createReadStream } from 'fs-extra';
+import { readdir, createReadStream, createWriteStream, mkdir } from 'fs-extra';
+import got from 'got';
 import program from 'commander'
 
 import isXMLDocumentBudgetaire from '../src/isXMLDocumentBudgetaire.js'
@@ -10,6 +11,10 @@ import { version } from '../package.json'
 
 function documentBudgetaireSummaryToPlanDeCompteURL({norme, sousNorme, année}){
     return `http://odm-budgetaire.org/composants/normes/${année}/${norme}/${sousNorme}/planDeCompte.xml`
+}
+
+function documentBudgetaireSummaryToFileName({norme, sousNorme, année}){
+    return `plan-de-compte-${norme}-${sousNorme}-${année}.xml`
 }
 
 
@@ -30,7 +35,23 @@ if (!inDir || !outDir) {
 
 console.log('in', inDir, 'out', outDir)
 
-readdir(inDir)
+const existingFilesInOutDirP = readdir(outDir)
+.catch(err => {
+    if(err.code === 'ENOENT'){
+        console.warn(`Le dossier de sortie n'existe pas, il va être créé`)
+        return mkdir(
+            isAbsolute(outDir) ? outDir : join(process.env.PWD, outDir), 
+            { recursive: true }
+        )
+        .then(() => [])
+    }
+    else throw err;
+})
+.then(files => new Set(files))
+
+
+
+const neededPlanDeCompteByURLP = readdir(inDir)
 .then(files => {
     const absoluteFiles = files.map(f => isAbsolute(f) ? f : join(inDir, f))
 
@@ -47,9 +68,47 @@ readdir(inDir)
     return Promise.all(docBudgFiles.map(f => makeDocumentBudgetaireSummary(createReadStream(f))))
     .then(summaries => new Map(summaries.map(s => [documentBudgetaireSummaryToPlanDeCompteURL(s), s])))
     .then(summaryByURL => {
-        console.log('urls:\n', [...summaryByURL.keys()].join('\n'))
+        console.log('Plans de compte nécessaires:')
+        console.log([...summaryByURL.values()].map(({norme, sousNorme, année}) => {
+            return `${norme} - ${sousNorme} - ${année}`
+        }).join('\n'))
+        return summaryByURL
     })
 })
 
+
+Promise.all([neededPlanDeCompteByURLP, existingFilesInOutDirP])
+.then(([neededPlanDeCompteByURL, existingFilesInOutDir]) => {
+    const neededPlanDeCompteFileNamesAndURLs = [...neededPlanDeCompteByURL.entries()]
+    .map(([url, summary]) => ({url, filename: documentBudgetaireSummaryToFileName(summary)}))
+    
+    const absentNeededPlanDeComptes = neededPlanDeCompteFileNamesAndURLs
+        .filter(({filename}) => !existingFilesInOutDir.has(filename))
+
+    console.log('Fichiers manquants :')
+    console.log([...absentNeededPlanDeComptes.map(({filename}) => filename)].join('\n'))
+
+    return Promise.all(absentNeededPlanDeComptes.map(({url, filename}) => {
+        const absoluteFilename = join(
+            isAbsolute(outDir) ? '' : process.env.PWD,
+            outDir,
+            filename
+        );
+
+        return new Promise((resolve, reject) => {
+            const fileStream = createWriteStream(absoluteFilename);
+            fileStream.on('finish', resolve)
+            fileStream.on('error', reject)
+
+            const networkStream = got.stream(url)
+
+            networkStream.on('error', reject)
+
+            networkStream.pipe(fileStream)
+        })
+    }))
+
+})
+.then(() => console.log(`Ayé, c'est fini !`))
 
 
